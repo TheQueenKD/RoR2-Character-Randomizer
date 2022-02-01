a2dd8b7991dec04aa80b7c4ff0e2cf07ff818178
TheQueenKD <kaitlynec96@gmail.com>
2022-01-31 21:48:28 -0500

    feat: prints random survivor to console

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Nathan\'s woman')
+import random
+
+
+CHARACTERS = [
+    'captain',
+    'engineer',
+    'rex',
+    'acrid',
+    'artificer',
+    'bandit',
+    'commando',
+    'huntress',
+    'loader',
+    'mercenary',
+    'mul-t',
+]
+
+
+def randomizer():
+    survivor = random.choice(CHARACTERS)
+    print(survivor)
+
+
+if __name__ == '__main__':
+    randomizer()
