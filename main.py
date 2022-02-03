fe788891aae184c53ec77311007924f8d733d42c
TheQueenKD <kaitlynec96@gmail.com>
2022-02-02 22:18:41 -0500

    feat: added 'You should play:' text and capitalized survivors

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ import random
 
 
 CHARACTERS = [
-    'captain',
-    'engineer',
-    'rex',
-    'acrid',
-    'artificer',
-    'bandit',
-    'commando',
-    'huntress',
-    'loader',
-    'mercenary',
-    'mul-t',
+    'Captain',
+    'Engineer',
+    'Rex',
+    'Acrid',
+    'Artificer',
+    'Bandit',
+    'Commando',
+    'Huntress',
+    'Loader',
+    'Mercenary',
+    'MUL-T',
 ]
 
 
-def randomizer():
-    survivor = random.choice(CHARACTERS)
-    print(survivor)
+def get_survivor():
+    return random.choice(CHARACTERS)
 
 
 if __name__ == '__main__':
-    randomizer()
+    print(f'You should play: {get_survivor()}')
